d0f2e8a34701684cf5bd1527cc981e72ddf2b48b
开通 王 <kaitong1991@outlook.com>
2017-01-09 16:43:38 +0800

    'stumansys'

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,11 @@
  * Created by User on 2017/1/6.
  */
 
+
+import '../css/main.scss'
+
+
+
 var sub = require('./sub');
 var app  = document.createElement('div');
 app.innerHTML = '<h1>Hello World!</h1>';
--- src/js/sub.js
+++ src/js/sub.js
@@ -4,7 +4,7 @@
 //我们这里使用CommonJS的风格
 function generateText() {
     var element = document.createElement('h2');
-    element.innerHTML = "Hello h2 world lalal";
+    element.innerHTML = "Hello h2 world@@@@";
     return element;
 }
 

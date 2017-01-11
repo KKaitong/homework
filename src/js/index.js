02b7bcceb5e7c7eab544d63e05b39ba61bbfe956
开通 王 <kaitong1991@outlook.com>
2017-01-11 09:32:08 +0800

    w

--- src/js/index.js
+++ src/js/index.js
@@ -5,10 +5,13 @@
 
 import '../css/main.scss'
 
-
-
 var sub = require('./sub');
 var app  = document.createElement('div');
+var $ = require('jquery');
+var moment = require('moment');
+
 app.innerHTML = '<h1>Hello World!</h1>';
 app.appendChild(sub());
 document.body.appendChild(app);
+
+$('body').append('<p>look at me! now is ' + moment().format() + '</p>');

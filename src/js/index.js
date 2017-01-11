96002b6fe01f057c6a48a1c81c513d1ddbf3ae9b
开通 王 <kaitong1991@outlook.com>
2017-01-11 09:37:20 +0800

    w

--- src/js/index.js
+++ src/js/index.js
@@ -4,14 +4,15 @@
 
 
 import '../css/main.scss'
+import generateText from './sub';
+import $ from 'jquery';
+import moment from 'moment';
 
-var sub = require('./sub');
-var app  = document.createElement('div');
-var $ = require('jquery');
-var moment = require('moment');
-
-app.innerHTML = '<h1>Hello World!</h1>';
-app.appendChild(sub());
+let app  = document.createElement('div');
+const myPromise = Promise.resolve(42);
+myPromise.then((number) => {
+    $('body').append('<p>promise result is ' + number + ' now is ' + moment().format() + '</p>');
+});
+app.innerHTML = '<h1>Hello World it</h1>';
 document.body.appendChild(app);
-
-$('body').append('<p>look at me! now is ' + moment().format() + '</p>');
+app.appendChild(generateText());
--- src/js/sub.js
+++ src/js/sub.js
@@ -1,11 +1,19 @@
 /**
  * Created by User on 2017/1/6.
  */
+
+
 //我们这里使用CommonJS的风格
-function generateText() {
+// function generateText() {
+//     var element = document.createElement('h2');
+//     element.innerHTML = "Hello h2 world@@@@";
+//     return element;
+// }
+//
+// module.exports = generateText;
+
+export default function  () {
     var element = document.createElement('h2');
-    element.innerHTML = "Hello h2 world@@@@";
+    element.innerHTML = "Hello h2 world hahaha";
     return element;
 }
-
-module.exports = generateText;
